681d2a17eb5aa3c450db919d1898fe9c13462924
Jocelyn Martinez <jocmartinez23@gmail.com>
2024-08-01 14:31:37 -0700

    jocelyn

--- FinalProject/CombinationSkin.swift
+++ FinalProject/CombinationSkin.swift
@@ -9,7 +9,96 @@ import SwiftUI
 
 struct CombinationSkin: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(red: 0.914, green: 0.934, blue: 0.908)
+                .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                Text("COMBINATION SKIN")
+                    .font(.custom("American Typewriter", size: 32)).font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 360, height: 50)
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                
+                  
+                
+                Divider()
+                    .frame(height: 5.0)
+                    .overlay(.brown)
+                
+                    .padding(.bottom)
+                Text("Signs of Combination Skin")
+                    .font(.custom("American Typewriter", size: 25)).font(.largeTitle)                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))               .font(.custom("American Typewriter", size: 32))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 330, height: 40)
+                    .background(Color(.white))
+                    .cornerRadius(10)
+
+                Text("- Acne")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Text("- Shiney Skin")
+                    .fontWeight(.light)
+                Text("- Flaking")
+                    .fontWeight(.light)
+                Text("- Tight Skin or Roughness")
+                    .fontWeight(.light)
+                Text("Foods to Incorperate in Diet")
+                    .font(.custom("American Typewriter", size: 25)).font(.largeTitle)                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))                    .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 350, height: 35)
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .padding(.top)
+                    
+                
+                Text("1. Oatmeal")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Text("2. Coconut Oil")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Text("3. Avocados")
+                    .fontWeight(.light)
+                Text("4. Green Tea")
+                    .fontWeight(.light)
+                Divider()
+                    .frame(height: 6.0)
+                    .overlay(Color(red: 0.444, green: 0.366, blue: 0.293, opacity: 0.693))
+                Text("Efficient Natural Masks")
+                    .font(.custom("American Typewriter", size: 25)).font(.largeTitle)                    .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                    .padding(.vertical)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 300, height: 35)
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))
+                Text("1. Oatmeal Mask: Oatmeal can works as it absorbes oil on the surface of skin. Mix 1/2 cups of oats with 2 tbsps of warm water. Apply to face and remove after 5 minutes.").fontWeight(.light)
+                
+                Spacer()
+                Spacer()
+                Text("2. Sunflower Seed Oils: Oil helps improve hydration when applied to the face.").fontWeight(.light)
+                      
+                Spacer()
+                Spacer()
+                Text("3. Avocado Mask: Can help soothe dry skin. 1/2 avocado with 1/4 cup of plain greek yogurt, and 1 teaspoon of tumeric.")
+                    .fontWeight(.light)
+                
+                Image("plant2")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                
+            }.padding()
+          }
     }
 }
 
--- FinalProject/WetSkin.swift
+++ FinalProject/WetSkin.swift
@@ -9,7 +9,94 @@ import SwiftUI
 
 struct WetSkin: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(red: 0.914, green: 0.934, blue: 0.908)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading){
+                Text("Oily Skin")
+                    .font(.custom("American Typewriter", size: 32))
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                        .background(Color(.white))
+                    .cornerRadius(10)
+                
+                    
+                    
+                
+                
+                Divider()
+                    .padding()
+                    .frame(height: 5.0)
+                    .overlay(Color(red: 0.444, green: 0.366, blue: 0.293, opacity: 0.693))
+                    
+                 
+                Text("   Oily skin can be caused by various factors such as weather, stress, genetics, etc.")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom)
+                
+                    
+                Text("Foods to Incorporate in Diet")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))
+                    .background(Color(.white))
+                    .font(.custom("American Typewriter", size: 25))
+                    .cornerRadius(10)
+                Spacer()
+                Spacer()
+                Text("1. Green Vegtables")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Text("2. Whole Grains: Wheat, Oats, Corn")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Text("3. Fatty Fish")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.leading)
+                Divider()
+                    .frame(height: 6.0)
+                    .overlay(Color(red: 0.444, green: 0.366, blue: 0.293, opacity: 0.693))
+                
+                Text("Efficient Natural Remedies")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(Color(red: 0.5445967913, green: 0.5896605253, blue: 0.502061903))
+                    .font(Font.custom("American Typewriter", size: 25))
+                   .background(Color(.white))
+                                       
+                                       .cornerRadius(10)
+                Spacer()
+                Spacer()
+                Text("1. Egg White and Lemon: This mask will aim at tighten pores. Mix one egg white with a teaspoon of lemon. Apply to face and rise once dried.")
+                    .fontWeight(.light)
+                    .padding(.bottom)
+                Text("2. Honey: Honey works as an antibacterial. Apply a thin layer to skin and leave for 10 minutes. Rise with warm water.")
+                    .fontWeight(.light)
+                    .padding(.bottom)
+                Text("3. Aloe Vera: Although good for sun burns, aloe vera improves oily skin. Apply a layer onto skin before bed and rinse in the morning.")
+                    .fontWeight(.light)
+                    .padding(.bottom)
+                
+                VStack(alignment: .trailing){
+                    Image("growth")
+                        .resizable(capInsets: EdgeInsets())
+                        .aspectRatio(contentMode: .fit)
+                    
+                    
+                }
+                .padding([.top, .leading, .bottom])
+            }
+            .padding(.horizontal, 20)
+
+            
+            
+        }
     }
 }
 
